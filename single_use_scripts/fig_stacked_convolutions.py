3cdc3fbb8e149ada8007b2989e9c7b7ba48f8046
michaelcbrown <mike.charles.brown@gmail.com>
2023-09-04 22:51:08 -0400

    fixing large file size

--- single_use_scripts/fig_stacked_convolutions.py
+++ single_use_scripts/fig_stacked_convolutions.py
@@ -5,27 +5,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def get_data():
+def get_data(folder):
     files_df = pd.read_csv(folder + '/files_df.csv')
     cells_df = pd.read_csv(folder + '/cells_df.csv')
     intensities_df = pd.read_csv(folder + '/intensities_df.csv')
     spike_times = pd.read_csv(folder + '/spike_times.csv')
-
     return (files_df
         .merge(cells_df[['cell', 'response_type', 'condition']], on='cell')
         .merge(spike_times, on='file_name')
         .merge(intensities_df[['cell','intensity','gauss_score']], on=['cell','intensity'])
     )
 
-def stacked_plots(df, show=True):
-    groups = (df.groupby('intensity', as_index=False))
+def f1(cell, cell_group):
+    groups = (cell_group.groupby('intensity', as_index=False))
     fig, axes = plt.subplots(groups.ngroups, 1, figsize=[10,8])
+    waves = []
 
     for ax, (intensity, group) in zip(fig.get_axes(), groups):
         sweep_length = int(group.sweep_length.max())
         flash_onset = group.flash_onset.max().round(5)
         group.spike_time += (flash_onset - group.flash_onset)
         time, gauss = gaussian_convolution(group.spike_time, sweep_length)
+
+        info = pd.Series({'cell':cell, 'intensity':intensity, 'flash_onset':flash_onset})
+        waves.append(pd.concat((info, pd.Series(data=gauss, index=time).loc[0:4])))
 
         score = group.gauss_score.iloc[0] == 3
         colors = {0: 'orange', 1: 'blue'}
@@ -45,20 +48,16 @@ def stacked_plots(df, show=True):
         sns.despine(ax=ax, left=True, bottom=True)
         ax.set_xticks([])
 
-    fig.suptitle(f"{df.cell.iloc[0]}, {df.response_type.iloc[0]}")
+    fig.suptitle(cell)
     plt.tight_layout()
-    if show:
-        plt.show()
+    plt.savefig(f'ESM figures/stacked_convolutions/{cell}')
+    plt.clf()
+    pd.concat(waves, axis=1).to_csv(f'ESM figures/stacked_convolutions/{cell}_waves', header=False)
 
+def f2(folder):
+    df = get_data(folder)
+    for cell, group in df.groupby('cell'):
+        f1(cell, group)
 
-folder = 'dfs_ESM'
-df = get_data()
-
-if 1:
-    for cell in df.cell.unique():
-        stacked_plots(df[df.cell==cell], show=False)
-        plt.savefig(f'figures2/stacked_convolutions/{cell}')
-        plt.clf()
-
-if 0:
-    stacked_plots(df[df.cell=='H21021901'])
+folder = "dfs_ESM"
+result = f2(folder)
